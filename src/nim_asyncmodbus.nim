1652e820f5bd0bf8d9194dfa5184d17b80942cd0
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2025-09-12 19:57:22 +0900

    update version.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- src/nim_asyncmodbus.nim
+++ src/nim_asyncmodbus.nim
@@ -11,12 +11,12 @@ when isMainModule:
     let coils = await self.readBits(1, 8)
     echo coils
 
-  proc test_modbus(self: ModbusCtx) {.async.} =
+  proc testModbus(self: ModbusCtx) {.async.} =
     await self.readDoValues()
     let status = await self.readInputBits(1, 8)
     echo status
-    let input_regs = await self.readInputRegisters(30001, 17)
-    echo input_regs
+    let inputRegs = await self.readInputRegisters(30001, 17)
+    echo inputRegs
     echo "--- set do0 --> on"
     discard await self.writeBit(1, true)
     await self.readDoValues()
@@ -29,10 +29,10 @@ when isMainModule:
     let rtu = newModbusRtu("/dev/tnt0", 19200)
     discard rtu.seSlave(2)
     discard rtu.connect()
-    await rtu.test_modbus()
+    await rtu.testModbus()
 
     echo "--- Test Modbus/TCP ---"
     let tcp = newModbusTcp("172.16.1.29", 502)
-    await tcp.test_modbus()
+    await tcp.testModbus()
 
   waitFor asyncMain()
