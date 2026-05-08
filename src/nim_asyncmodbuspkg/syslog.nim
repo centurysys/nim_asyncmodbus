a5a07f20c8a0ee86e85aacf38acd324c157cf5ad
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2026-05-08 20:28:00 +0900

    fix(syslog): pass log messages as data
    Call syslog() with a fixed "%s" format string so message contents are
    not interpreted as a C format string.
    
    This prevents percent sequences in log messages from being treated as
    format specifiers.
    
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- src/nim_asyncmodbuspkg/syslog.nim
+++ src/nim_asyncmodbuspkg/syslog.nim
@@ -7,26 +7,26 @@ const
   LOG_NOTICE* = 5
   LOG_INFO* = 6
   LOG_DEBUG* = 7
-  LOG_KERN* = (0 shl 3)           ##  kernel messages
-  LOG_USER* = (1 shl 3)           ##  random user-level messages
-  LOG_MAIL* = (2 shl 3)           ##  mail system
-  LOG_DAEMON* = (3 shl 3)         ##  system daemons
-  LOG_AUTH* = (4 shl 3)           ##  security/authorization messages
-  LOG_SYSLOG* = (5 shl 3)         ##  messages generated internally by syslogd
-  LOG_LPR* = (6 shl 3)            ##  line printer subsystem
-  LOG_NEWS* = (7 shl 3)           ##  network news subsystem
-  LOG_UUCP* = (8 shl 3)           ##  UUCP subsystem
-  LOG_CRON* = (9 shl 3)           ##  clock daemon
-  LOG_AUTHPRIV* = (10 shl 3)      ##  security/authorization messages (private)
-  LOG_FTP* = (11 shl 3)           ##  ftp daemon
-  LOG_LOCAL0* = (16 shl 3)        ##  reserved for local use
-  LOG_LOCAL1* = (17 shl 3)        ##  reserved for local use
-  LOG_LOCAL2* = (18 shl 3)        ##  reserved for local use
-  LOG_LOCAL3* = (19 shl 3)        ##  reserved for local use
-  LOG_LOCAL4* = (20 shl 3)        ##  reserved for local use
-  LOG_LOCAL5* = (21 shl 3)        ##  reserved for local use
-  LOG_LOCAL6* = (22 shl 3)        ##  reserved for local use
-  LOG_LOCAL7* = (23 shl 3)        ##  reserved for local use
+  LOG_KERN* = (0 shl 3) ## kernel messages
+  LOG_USER* = (1 shl 3) ## random user-level messages
+  LOG_MAIL* = (2 shl 3) ## mail system
+  LOG_DAEMON* = (3 shl 3) ## system daemons
+  LOG_AUTH* = (4 shl 3) ## security/authorization messages
+  LOG_SYSLOG* = (5 shl 3) ## messages generated internally by syslogd
+  LOG_LPR* = (6 shl 3) ## line printer subsystem
+  LOG_NEWS* = (7 shl 3) ## network news subsystem
+  LOG_UUCP* = (8 shl 3) ## UUCP subsystem
+  LOG_CRON* = (9 shl 3) ## clock daemon
+  LOG_AUTHPRIV* = (10 shl 3) ## security/authorization messages (private)
+  LOG_FTP* = (11 shl 3) ## ftp daemon
+  LOG_LOCAL0* = (16 shl 3) ## reserved for local use
+  LOG_LOCAL1* = (17 shl 3) ## reserved for local use
+  LOG_LOCAL2* = (18 shl 3) ## reserved for local use
+  LOG_LOCAL3* = (19 shl 3) ## reserved for local use
+  LOG_LOCAL4* = (20 shl 3) ## reserved for local use
+  LOG_LOCAL5* = (21 shl 3) ## reserved for local use
+  LOG_LOCAL6* = (22 shl 3) ## reserved for local use
+  LOG_LOCAL7* = (23 shl 3) ## reserved for local use
   LOG_PID* = 0x00000001
   LOG_CONS* = 0x00000002
   LOG_ODELAY* = 0x00000004
@@ -36,9 +36,9 @@ const
 
 proc c_closelog() {.importc: "closelog", header: "syslog.h".}
 proc c_openlog(ident: cstring; option: cint; facility: cint) {.importc: "openlog",
-    header: "syslog.h".}
+  header: "syslog.h".}
 proc c_syslog(pri: cint; fmt: cstring) {.varargs, importc: "syslog",
-    header: "syslog.h".}
+  header: "syslog.h".}
 
 const
   defaultIdent = ""
@@ -54,7 +54,7 @@ proc openlog*(ident: string = defaultIdent, facility: int = defaultFacility) =
 #
 # ------------------------------------------------------------------------------
 proc syslog*(pri: cint, msg: string) =
-  c_syslog(pri, msg)
+  c_syslog(pri, "%s", msg.cstring)
 
 # ------------------------------------------------------------------------------
 #
@@ -109,7 +109,6 @@ proc warn*(msg: string) =
 # ------------------------------------------------------------------------------
 proc warning*(msg: string) =
   syslog(LOG_WARNING, msg)
-
 
 when isMainModule:
   openlog("SyslogTest")
